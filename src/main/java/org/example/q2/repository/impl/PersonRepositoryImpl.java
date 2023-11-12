79f0533af5eafbe20630acc47056d97d89144898
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-11-12 12:39:09 +0330

    create constructor

--- src/main/java/org/example/q2/repository/impl/PersonRepositoryImpl.java
+++ src/main/java/org/example/q2/repository/impl/PersonRepositoryImpl.java
@@ -2,10 +2,16 @@ package org.example.q2.repository.impl;
 
 import org.example.q2.entity.Person;
 import org.example.q2.repository.PersonRepository;
+import org.hibernate.Session;
 
 import java.util.List;
 
 public class PersonRepositoryImpl implements PersonRepository {
+
+    Session session;
+    public PersonRepositoryImpl(Session session){
+        this.session=session;
+    }
     @Override
     public void save() {
 
