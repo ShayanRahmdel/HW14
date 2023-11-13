6ecf77d095ec0a3d44a78e17c9952f985517352d
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-11-13 12:07:15 +0330

    create getpersonService

--- src/main/java/org/example/q2/util/AppContext.java
+++ src/main/java/org/example/q2/util/AppContext.java
@@ -3,14 +3,24 @@ package org.example.q2.util;
 import org.example.q2.connection.SessionFactoryProvider;
 import org.example.q2.repository.PersonRepository;
 import org.example.q2.repository.impl.PersonRepositoryImpl;
+import org.example.q2.service.PersonService;
+import org.example.q2.service.impl.PersonServiceImpl;
 import org.hibernate.Session;
 
 public class AppContext {
         private static Session SISSION;
         private static final PersonRepository PERSON_REPOSITORY;
 
+        private static final PersonService PERSON_SERVICE;
+
     static {
         SISSION = SessionFactoryProvider.getSessionFactory().openSession();
         PERSON_REPOSITORY = new PersonRepositoryImpl(SISSION);
+        PERSON_SERVICE = new PersonServiceImpl(PERSON_REPOSITORY);
     }
+
+    public static PersonService getPersonService(){
+        return PERSON_SERVICE;
+    }
+
 }
