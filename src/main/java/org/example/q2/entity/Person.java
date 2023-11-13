b509e6ab5e3e5378d3e2b252d918ad04fdfdfec0
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-11-13 12:07:30 +0330

    create constructor

--- src/main/java/org/example/q2/entity/Person.java
+++ src/main/java/org/example/q2/entity/Person.java
@@ -11,8 +11,13 @@ import java.util.Date;
 @Setter
 @Getter
 @ToString
-@Table(name = "Person")
 public class Person {
+    public Person(Long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
