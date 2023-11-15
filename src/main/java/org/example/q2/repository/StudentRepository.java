bc1475bd6da456d6036ee6b49ba650e090c3e9ad
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-11-15 15:59:30 +0330

    create Student repository

---
+++ src/main/java/org/example/q2/repository/StudentRepository.java
@@ -0,0 +1,19 @@
+package org.example.q2.repository;
+
+import org.example.q2.entity.Person;
+import org.example.q2.entity.Student;
+
+import java.util.List;
+
+public interface StudentRepository {
+    void save(Student student);
+    Student loadById(Long id);
+
+    void update(Student student);
+
+    void delete(Student student);
+
+    List<Student> loadAll();
+
+    Boolean contains(String firstName);
+}
---
+++ src/main/java/org/example/q2/repository/impl/StudentRepositoryImpl.java
@@ -0,0 +1,93 @@
+package org.example.q2.repository.impl;
+
+import org.example.q2.entity.Person;
+import org.example.q2.entity.Student;
+import org.example.q2.repository.StudentRepository;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class StudentRepositoryImpl implements StudentRepository {
+    Session session;
+
+    public StudentRepositoryImpl(Session session) {
+        this.session = session;
+    }
+
+    @Override
+    public void save(Student student) {
+        try {
+            session.beginTransaction();
+            session.persist(student);
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+
+
+    }
+
+    @Override
+    public Student loadById(Long id) {
+        try {
+            session.beginTransaction();
+            return session.load(Student.class,id);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void update(Student student) {
+
+        try {
+            session.beginTransaction();
+            session.update(student);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+
+
+    }
+
+    @Override
+    public void delete(Student student) {
+
+        try {
+            session.beginTransaction();
+            Person person1 = session.get(Person.class, student.getId());
+            session.delete(person1);
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+
+    }
+
+    @Override
+    public List<Student> loadAll() {
+        String hql = "select f from Student f";
+        Query<Student> query = session.createQuery(hql, Student.class);
+        return query.list();
+
+    }
+
+    @Override
+    public Boolean contains(String firstName) {
+        String hql = "select count(*) from Student p where p.firstName = :firstName";
+        Query<Long> query = session.createQuery(hql, Long.class);
+        query.setParameter("firstName",firstName);
+        Long aLong = query.uniqueResult();
+        return aLong !=null && aLong>0;
+    }
+}
